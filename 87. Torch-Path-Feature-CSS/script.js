2e8525286cbafd324afd3def805b843118ba1e55
Hansraj Parmar <110182495+hansrajparmar@users.noreply.github.com>
2024-02-26 09:41:54 +0530

    Update script.js

--- 87. Torch-Path-Feature-CSS/script.js
+++ 87. Torch-Path-Feature-CSS/script.js
@@ -1,5 +1,5 @@
 let gurucodes = document.documentElement;
-
+ 
 gurucodes.addEventListener("mousemove", (e) => {
   gurucodes.style.setProperty("--1", e.clientX + "px");
   gurucodes.style.setProperty("--2", e.clientY + "px");
